b1b783fb48e64a34d2ef27c044251684f4877502
Hudson Lima <hudsonflima@gmail.com>
2024-05-22 16:01:50 -0300

    Update description.js

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
             <footer className="rounded-lg m-4 bg-gray-800/50 shadow-sm shadow-white/80">
                 <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
                     <span className="text-sm sm:text-center dark:text-gray-400">
-                        © {currentYear} Desenvolvido por NextBI Development.
+                        © {currentYear} Desenvolvido por Hudson Lima.
                     </span>
                     <span className='text-sm sm:text-center dark:text-gray-400'>Todos os direitos reservados.</span>
                 </div>
